eecd07db63391f6968da1c6c8b044abeb00a89ed
stevesansford <152357970+stevesansford@users.noreply.github.com>
2024-10-29 11:13:33 -0400

    Added diagonal pawn captures.

--- sherwood.js
+++ sherwood.js
@@ -7,25 +7,33 @@
 // White Knight: 2      Black Knight: -2
 // White Pawn: 1        Black Pawn: -1
 
+// TODO List:
+// 1. Rewrite the ChessBoard an an object (constructor funtion) with props for board state and position.
+// 1.1. Use 'New' syntax.
+// 2. Rewrite variuos functions as methods on the ChessBoard object.
 
 
 
+// create the chess board object
+const ChessBoard = {
+    position: Array(128),
+    sideToMove: 'w',
+    castling: 'KQkq',
+    enPassant: '-',
+    movesMade: 0,
+}
 
-
-let chessBoard = new Array(128).fill(null);
-
-for (i = 0; i < 128; i++) {
-    if ((i & 0x88) === 0) {
-        chessBoard[i] = 0;
-        //console.log(squareToCoords(i));
+// mark the playable squares on the board with '0'
+function initializeBoard() {
+    for (i = 0; i < 128; i++) {
+        if ((i & 0x88) === 0) {
+            ChessBoard[i] = 0;
+        }
     }
 }
 
-
+// convert the index of the array to human-readable algebraic notation
 function squareToCoords(square) {
-    if ((i & 0x88) === 0) 
-        return "Off Board";
-    
     let file = square % 16;
     let rank = Math.floor(square / 16);
 
@@ -35,6 +43,7 @@ function squareToCoords(square) {
     return fileChar + rowChar;
 }
 
+// display the board in the console
 function displayBoard() {
     const pieceSymbols = {
         1: 'P',   // White Pawn
@@ -58,13 +67,14 @@ function displayBoard() {
         let rowString = (row + 1) + " "; // Row numbers
         for (let col = 0; col < 8; col++) {
             const index = row * 16 + col;
-            rowString += pieceSymbols[chessBoard[index]] + " ";
+            rowString += pieceSymbols[ChessBoard[index]] + " ";
         }
         console.log(rowString);
     }
     console.log("  a b c d e f g h\n"); // Column headers again for clarity
 }
 
+// parse a FEN string and arrange the peices on the board
 function parseFEN(fen) {
     const piecePlacement = fen.split(" ")[0]; // Get the piece placement part of the FEN
 
@@ -78,32 +88,33 @@ function parseFEN(fen) {
         } else {
             // Place piece on 0x88 board
             const index = row * 16 + col; // Calculate 0x88 index
-            if (char === 'k') chessBoard[index] = -6;
-            else if (char === 'q') chessBoard[index] = -5;
-            else if (char === 'r') chessBoard[index] = -4;
-            else if (char === 'b') chessBoard[index] = -3;
-            else if (char === 'n') chessBoard[index] = -2;
-            else if (char === 'p') chessBoard[index] = -1;
-            else if (char === 'K') chessBoard[index] = 6;
-            else if (char === 'Q') chessBoard[index] = 5;
-            else if (char === 'R') chessBoard[index] = 4;
-            else if (char === 'B') chessBoard[index] = 3;
-            else if (char === 'N') chessBoard[index] = 2;
-            else if (char === 'P') chessBoard[index] = 1;
-            else chessBoard[index] = 0;
+            if (char === 'k') ChessBoard[index] = -6;
+            else if (char === 'q') ChessBoard[index] = -5;
+            else if (char === 'r') ChessBoard[index] = -4;
+            else if (char === 'b') ChessBoard[index] = -3;
+            else if (char === 'n') ChessBoard[index] = -2;
+            else if (char === 'p') ChessBoard[index] = -1;
+            else if (char === 'K') ChessBoard[index] = 6;
+            else if (char === 'Q') ChessBoard[index] = 5;
+            else if (char === 'R') ChessBoard[index] = 4;
+            else if (char === 'B') ChessBoard[index] = 3;
+            else if (char === 'N') ChessBoard[index] = 2;
+            else if (char === 'P') ChessBoard[index] = 1;
+            else ChessBoard[index] = 0;
              col++;
         }
     }
 }
 
+// generate all pseudo-legal moves in the current position
 function generateMoves() {
     const moves = [];
     let moveCounter = 0;
 
     for (let square = 0; square < 128; square++) {
-        if ((square & 0x88) !== 0 || chessBoard[square] === null) continue; // Skip invalid or empty squares
+        if ((square & 0x88) !== 0 || ChessBoard[square] === null) continue; // Skip invalid or empty squares
 
-        const piece = chessBoard[square];
+        const piece = ChessBoard[square];
         const absPiece = Math.abs(piece);
         let pawnCaptures = [];
         let pieceOffsets = [];
@@ -111,7 +122,6 @@ function generateMoves() {
         //get white pawn moves
         if (piece === 1) {
             pieceOffsets = [16];
-            pawnCapture = [15, 17];   
             if (Math.floor(square / 8) === 2 ) {
                 pieceOffsets.push(32);
             }
@@ -120,7 +130,6 @@ function generateMoves() {
         //get black pawn moves
         if (piece === -1) {
             pieceOffsets = [-16];
-            pawnCapture = [-15, -17];   
             if (Math.floor(square / 8) === 12 ) {
                 pieceOffsets.push(-32);
             }
@@ -153,17 +162,40 @@ function generateMoves() {
 
         for (let offset of pieceOffsets) {
             let target = square + offset;
-            while ((target & 0x88) === 0) { // Valid chessBoard square
-                if  (chessBoard[target] === 0) {
-                    moves.push(squareToCoords(square)+''+squareToCoords(target)); // Add empty square move
-                    moveCounter++;
-                } else {
-                    if (( absPiece !== 1 )) {
-                        if (( chessBoard[target] > 0) !== (piece > 0)) { // Capture if opposite color but no pawn captures.
-                            moves.push(squareToCoords(square)+''+squareToCoords(target)+"::" + piece + "x" + chessBoard[target] );
+            const fromSquare = squareToCoords(square);
+            const toSquare = squareToCoords(target);
+
+            while ((target & 0x88) === 0) { // Valid ChessBoard square
+                //check for diagonal pawn captures
+                if (absPiece === 1) {
+                    if (ChessBoard[target - 1] !== 0) {
+                        if (( ChessBoard[target - 1] > 0) !== (piece > 0)) { // Capture if opposite color
+                            moves.push(fromSquare + squareToCoords(target - 1) + ' ' + piece + "x" + ChessBoard[target - 1] );
                             moveCounter++;
                         }
-                    } 
+                    }
+                    if (ChessBoard[target + 1] !== 0) {
+                        if (( ChessBoard[target + 1] > 0) !== (piece > 0)) { // Capture if opposite color
+                            moves.push(fromSquare + squareToCoords(target + 1) + ' ' + piece + "x" + ChessBoard[target + 1] );
+                            moveCounter++;
+                        }
+                    }
+                }
+                
+                //check for moves to empy squares
+                if (ChessBoard[target] === 0) {
+                    moves.push(fromSquare + toSquare); // Add empty square move
+                    moveCounter++;
+                } 
+
+                //check for captures
+                if (ChessBoard[target] !== 0) { 
+                    if ( absPiece !== 1 ) { //prevent pawn from caputing forward
+                        if (( ChessBoard[target] > 0) !== (piece > 0)) { // Capture if opposite color
+                            moves.push(fromSquare + toSquare + ' ' + piece + "x" + ChessBoard[target] );
+                            moveCounter++;
+                        }
+                    }
                     break; // Stop if blocked by any piece
                 }
                 if ([1, 2, 6].includes(absPiece)) break; // Stop if non-sliding piece (Pawn, Knight, King)
@@ -175,10 +207,17 @@ function generateMoves() {
     return moves;
 }
 
-const fenStart = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-const fenTest = "r6r/1b2k1bq/8/8/7B/8/8/R3K2R b KQ - 3 2";
-const board0x88 = parseFEN(fenTest);
+// RUN THE APP
+// -----------
 
+// Set our starting FEN
+//const fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+//const fen = "r6r/1b2k1bq/8/8/7B/8/8/R3K2R b KQ - 3 2";
+const fen = "8/8/8/4p3/3N4/8/8/8 w KQkq - 0 1";
+
+// Run the app
+initializeBoard();
+parseFEN(fen);
 displayBoard();
 
 console.log(generateMoves());
